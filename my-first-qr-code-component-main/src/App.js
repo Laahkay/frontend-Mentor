f60e0fe7c31aeb01c473376a3fa8af5dd7b40ac7
Laahkay <lonerkiddow15@gmail.com>
2023-02-17 10:37:25 +0200

    adding react projects on what i have learned in frontend mento

---
+++ my-first-qr-code-component-main/src/App.js
@@ -0,0 +1,17 @@
+// import image from "./images/image-qr-code.png"
+import './App.css';
+
+function App() {
+  return (
+    <div className="myDiv">
+      <img id="qr-code" src="./image-qr-code.png" alt="QR code" />
+      <h1>Improve your front-end skills by building projects</h1>
+
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills
+        to
+        the next level</p>
+      </div>
+  );
+}
+
+export default App;
